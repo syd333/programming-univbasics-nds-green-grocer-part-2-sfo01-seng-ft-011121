a8858e2636333fa2ca1d64565d3fcfb696a18f8d
sydney miranda <sydmiranda@icloud.com>
2021-01-09 01:38:48 +0000

    Automatically backed up by Learn

--- lib/grocer_part_2.rb
+++ lib/grocer_part_2.rb
@@ -60,4 +60,5 @@ def checkout(cart, coupons)
   consolidated_cart = consolidate_cart(cart)
   couponed_cart = apply_coupons(consolidated_cart)
   clearanced_cart = apply_clearance(couponed_cart)
+  
 end
