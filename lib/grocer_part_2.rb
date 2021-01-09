677e7079f79de935301bdd921d8191f86982ee29
sydney miranda <sydmiranda@icloud.com>
2021-01-09 00:55:56 +0000

    Automatically backed up by Learn

--- lib/grocer_part_2.rb
+++ lib/grocer_part_2.rb
@@ -7,7 +7,7 @@ def apply_coupons(cart, coupons)
   # REMEMBER: This method **should** update cart
    counter = 0
    while counter < coupons.length
-     
+     cart_item = find_item_by_name_in_collection(coupons[counter][:item])
      counter += 1
    end
 end
