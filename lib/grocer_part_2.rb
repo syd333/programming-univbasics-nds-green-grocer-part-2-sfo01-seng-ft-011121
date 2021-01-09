cd89ac48d4a5b2417b7b71b1d502fe4434628543
sydney miranda <sydmiranda@icloud.com>
2021-01-09 01:38:41 +0000

    Automatically backed up by Learn

--- lib/grocer_part_2.rb
+++ lib/grocer_part_2.rb
@@ -58,4 +58,6 @@ def checkout(cart, coupons)
   # BEFORE it begins the work of calculating the total (or else you might have
   # some irritated customers
   consolidated_cart = consolidate_cart(cart)
+  couponed_cart = apply_coupons(consolidated_cart)
+  clearanced_cart = apply_clearance(couponed_cart)
 end
