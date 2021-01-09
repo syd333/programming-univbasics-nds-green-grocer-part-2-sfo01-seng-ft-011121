660b60782acb4c779ab3dc6053ce15dd3df822a0
sydney miranda <sydmiranda@icloud.com>
2021-01-09 01:23:30 +0000

    Automatically backed up by Learn

--- lib/grocer_part_2.rb
+++ lib/grocer_part_2.rb
@@ -14,6 +14,11 @@ def apply_coupons(cart, coupons)
        if cart_item_with_coupon
          cart_item_with_coupon[:count] += coupons[counter][:num]
          cart_item[:count] -= coupons[counter][:num]
+       else
+         cart_item_with_coupon = {
+           :item => couponed_item_name,
+           :price => 
+         }
        end
      end
       counter += 1
